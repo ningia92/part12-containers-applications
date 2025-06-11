9129edf53297c0e4984ee2675e9fc796c2515067
ningia <113545072+ningia92@users.noreply.github.com>
2025-06-11 23:06:17 +0200

    Add implementation of routes to get and update one todo

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,13 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  await req.todo.updateOne(req.body)
+  res.sendStatus(204)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
